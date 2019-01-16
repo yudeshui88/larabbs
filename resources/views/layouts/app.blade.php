fdbe44b7257834de6ca9501ff7ab23c45c2ebfcc
yudeshui <1124714824@qq.com>
2019-01-16 08:42:41 +0000

    更改编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -21,5 +22,6 @@
     </div>
 
     <script src="{{ mix('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
