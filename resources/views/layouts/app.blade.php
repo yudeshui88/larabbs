8eedf0233fa3056c3afa317aeffe5c6c33cc68e2
yudeshui <1124714824@qq.com>
2019-01-16 08:57:39 +0000

    帖子详情

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @yield('styles')
