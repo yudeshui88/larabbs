ab957b02694cce19409cf7158663b6def24bc6c5
yudeshui <1124714824@qq.com>
2019-01-16 02:04:55 +0000

    浏览器缓存问题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -19,6 +19,6 @@
         @include('layouts._footer')
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
